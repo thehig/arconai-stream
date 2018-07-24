690d52de9c5e1d73e68d451041a549a2596f683f
David Higgins <thehig@gmail.com>
2018-07-24 15:20:09 +0100

    Extract streams to separate file, add Game of Thrones

--- src/express.js
+++ src/express.js
@@ -1,25 +1,17 @@
 const express = require('express')
 const fetchScript = require('./fetch-script')
+const streams = require('./streams')
 
 const PORT = process.env.PORT || 3000
 
-const streams = [
-  { id: 138, name: 'Stargate' },
-  { id: 139, name: 'House' },
-  { id: 32, name: 'Community' },
-  { id: 70, name: 'Outer Limits' },
-  { id: 150, name: 'Rick & Morty' },
-  { id: 84, name: 'South Park' },
-  { id: 115, name: 'Supernatural' },
-  { id: 15, name: 'Last Airbender & Korra' }
-]
+
 
 var app = express()
 app.set('view engine', 'ejs')
 app.set('views', 'src/views')
 
 app.get('/', (req, res) => {
-  res.render('index', { streams })
+  res.render('index', streams)
 })
 
 app.get('/:streamid', (req, res) => {
