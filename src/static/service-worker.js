0989a7895be8fc41be1df9ba8ee6365060ada7b5
David Higgins <thehig@gmail.com>
2018-09-05 00:11:37 +0100

    Add ETag automagic reload

--- src/static/service-worker.js
+++ src/static/service-worker.js
@@ -1,7 +1,29 @@
-const DEBUG = true
+const DEBUG = false
 
 const CACHE_BASE_NAME = 'ARCONAI-STREAM'
-const CACHE_TAG = '0000'
+const CACHE_TAG = '0001'
+
+// #region Output Styles
+
+const swStyle = 'background: #333;' // Service Worker Background
+const fnStyle = 'background: #EEE;' // Function Background
+
+const styles = {
+  install: `${swStyle} color: #c8f7c5`,
+  activate: `${swStyle} color: #c5cbf7`,
+  delete: `${swStyle} color: #e0ffff`,
+  fetch: `${swStyle} color: #ffecdb`,
+
+  precache: `${fnStyle} color: #2e8856`,
+  fromNetwork: `${fnStyle} color: #638bb3`,
+  fromCache: `${fnStyle} color: #bf55ec`,
+  updateCache: `${fnStyle} color: #808080`,
+  refresh: `${fnStyle} color: #9d8319`
+}
+
+// #endregion
+
+// #region Cache Definitions
 
 // Static cache is loaded once and then always served from cache
 const staticCache = {
@@ -15,43 +37,58 @@ const staticCache = {
   ]
 }
 
-// Update cache is loaded, then when requested it serves from cache, and in the background updates and refreshes the client
+// Update cache is loaded, then when requested it serves from cache
+//    then in the background it tries to update the cache from network
+//    then refreshes the client
 const updateCache = {
   name: `${CACHE_BASE_NAME}-UPDATE-${CACHE_TAG}`,
   files: ['/']
 }
 
-// Accessors for named caches
-const fromStaticCache = fromUsingCache(staticCache.name)
-const fromUpdateCache = fromUsingCache(updateCache.name)
-const updateUpdateCache = updateUsingCache(updateCache.name)
-
 const CACHES = [staticCache, updateCache]
+
+// #endregion
+
+// #region Service Worker Lifecycle Events
 
 /**
  * Install service worker and runs precache
  */
 self.addEventListener('install', function(evt) {
-  if (DEBUG) console.log('install serviceworker')
+  if (DEBUG) console.log('%c[->][install]', styles.install)
+
   evt.waitUntil(precache())
 })
 
 /**
- * Populate cache with a set of resources
+ * Remove any caches other than the current ones
  */
-function precache() {
-  return Promise.all(
-    CACHES.map(function({ name, files }) {
-      return caches.open(name).then(function(cache) {
-        return cache.addAll(files)
-      })
+self.addEventListener('activate', function(event) {
+  if (DEBUG) console.log('%c[->][activate]', styles.activate)
+  event.waitUntil(
+    // Get all the cache names (keys)
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        // For each cache name
+        cacheNames.map(function(key) {
+          // Whitelist the current cache names
+          if (
+            CACHES.map(function(c) {
+              return c.name
+            }).indexOf(key) === -1
+          ) {
+            if (DEBUG) console.log('%c[->][delete]', styles.delete, key)
+            // Delete the rest
+            return caches.delete(key)
+          }
+        })
+      )
     })
   )
-}
+})
 
 self.addEventListener('fetch', function(evt) {
-  if (DEBUG)
-    console.log('%cfetch', 'background: #222; color: #bf6ee0', evt.request.url)
+  if (DEBUG) console.log('%c[->][fetch]', styles.fetch, evt.request.url)
 
   // If resource is in static cache, return it from the static cache
   // If resource is in update cache, return it from the update cache, then update the cache from the network and refresh the client
@@ -75,23 +112,27 @@ self.addEventListener('fetch', function(evt) {
   )
 })
 
-/**
- * Get a resource from a URL (within ${timeout}ms if specified)
- *
- * Returns resource
- * If timeout is specified, throws an error if timeout exceeded
- */
-function fromNetwork(request, timeout) {
-  if (DEBUG)
-    console.log('%cfromNetwork', 'color: #1e90ff', request.url, timeout)
+// #endregion
 
-  return new Promise(function(fulfill, reject) {
-    var timeoutId = timeout && setTimeout(reject, timeout)
-    fetch(request).then(function(response) {
-      timeout && clearTimeout(timeoutId)
-      fulfill(response)
-    }, reject)
-  })
+// #region Cache Functions
+
+// Accessors for named caches
+const fromStaticCache = fromUsingCache(staticCache.name)
+const fromUpdateCache = fromUsingCache(updateCache.name)
+const updateUpdateCache = updateUsingCache(updateCache.name)
+
+/**
+ * Populate cache with a set of resources
+ */
+function precache() {
+  if (DEBUG) console.log('%c[->][precache]', styles.precache)
+  return Promise.all(
+    CACHES.map(function({ name, files }) {
+      return caches.open(name).then(function(cache) {
+        return cache.addAll(files)
+      })
+    })
+  )
 }
 
 /**
@@ -106,8 +147,8 @@ function fromUsingCache(cacheName) {
       return cache.match(request).then(function(matching) {
         if (DEBUG && matching) {
           console.log(
-            '%cfromCache: ' + cacheName,
-            'color: #af851a',
+            `%c[<-][cache ${cacheName}]`,
+            styles.fromCache,
             request.url
           )
         }
@@ -125,7 +166,11 @@ function fromUsingCache(cacheName) {
 function updateUsingCache(cacheName) {
   return function update(request) {
     if (DEBUG)
-      console.log('%cupdate: ' + cacheName, 'color: #7f311a', request.url)
+      console.log(
+        `%c[->][update ${cacheName}]`,
+        styles.updateCache,
+        request.url
+      )
     return caches.open(cacheName).then(function(cache) {
       return fromNetwork(request).then(function(response) {
         return cache.put(request, response.clone()).then(function() {
@@ -136,20 +181,49 @@ function updateUsingCache(cacheName) {
   }
 }
 
+// #endregion
+
+/**
+ * Get a resource from a URL (within ${timeout}ms if specified)
+ *
+ * Returns resource
+ * If timeout is specified, throws an error if timeout exceeded
+ */
+function fromNetwork(request, timeout) {
+  return new Promise(function(fulfill, reject) {
+    var timeoutId = timeout && setTimeout(reject, timeout)
+    fetch(request).then(function(response) {
+      if (DEBUG)
+        console.log(
+          '%c[<-][network]',
+          styles.fromNetwork,
+          request.url,
+          timeout || ''
+        )
+      timeout && clearTimeout(timeoutId)
+      fulfill(response)
+    }, reject)
+  })
+}
+
 /**
  * Refresh any serviceworker connected client pages
  */
 function refresh(response) {
-  if (DEBUG) console.log('%crefresh: ', 'color: #21350A', response.url)
-  return self.clients.matchAll().then(function(clients) {
-    clients.forEach(function(client) {
-      var message = {
-        type: 'refresh',
-        url: response.url,
-        eTag: response.headers.get('ETag')
-      }
+  if (DEBUG) console.log('%c[<-][refresh]', styles.refresh, response.url)
 
-      client.postMessage(JSON.stringify(message))
-    })
+  return self.clients.matchAll().then(function(clients) {
+    return Promise.all(
+      clients.map(function(client) {
+        return new Promise(function(resolve) {
+          var message = {
+            type: 'refresh',
+            url: response.url,
+            eTag: response.headers.get('ETag')
+          }
+          resolve(client.postMessage(JSON.stringify(message)))
+        })
+      })
+    )
   })
 }
