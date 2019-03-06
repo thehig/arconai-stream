f76329954f99c6186ac55b196cc6e3d78538822e
David Higgins <thehig@gmail.com>
2019-03-06 23:06:34 +0000

    Replace function with () =>

--- src/static/service-worker.js
+++ src/static/service-worker.js
@@ -54,7 +54,7 @@ const CACHES = [staticCache, updateCache]
 /**
  * Install service worker and runs precache
  */
-self.addEventListener('install', function(evt) {
+self.addEventListener('install', evt => {
   if (DEBUG) console.log('%c[->][install]', styles.install)
 
   evt.waitUntil(precache())
@@ -63,31 +63,27 @@ self.addEventListener('install', function(evt) {
 /**
  * Remove any caches other than the current ones
  */
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', event => {
   if (DEBUG) console.log('%c[->][activate]', styles.activate)
   event.waitUntil(
     // Get all the cache names (keys)
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
+    caches.keys().then(cacheNames =>
+      Promise.all(
         // For each cache name
-        cacheNames.map(function(key) {
+        cacheNames.map(key => {
           // Whitelist the current cache names
-          if (
-            CACHES.map(function(c) {
-              return c.name
-            }).indexOf(key) === -1
-          ) {
+          if (CACHES.map(c => c.name).indexOf(key) === -1) {
             if (DEBUG) console.log('%c[->][delete]', styles.delete, key)
             // Delete the rest
             return caches.delete(key)
           }
         })
       )
-    })
+    )
   )
 })
 
-self.addEventListener('fetch', function(evt) {
+self.addEventListener('fetch', evt => {
   if (DEBUG) console.log('%c[->][fetch]', styles.fetch, evt.request.url)
 
   // If resource is in static cache, return it from the static cache
@@ -96,19 +92,19 @@ self.addEventListener('fetch', function(evt) {
 
   evt.respondWith(
     // Return from the static cache if valid
-    fromStaticCache(evt.request).catch(function() {
+    fromStaticCache(evt.request).catch(() =>
       // return from update cache if valid
-      return fromUpdateCache(evt.request)
-        .then(function(response) {
+      fromUpdateCache(evt.request)
+        .then(response => {
           // update and refresh
           evt.waitUntil(updateUpdateCache(evt.request).then(refresh))
           return response
         })
-        .catch(function() {
+        .catch(() =>
           // resort to network
-          return fromNetwork(evt.request)
-        })
-    })
+          fromNetwork(evt.request)
+        )
+    )
   )
 })
 
@@ -127,11 +123,9 @@ const updateUpdateCache = updateUsingCache(updateCache.name)
 function precache() {
   if (DEBUG) console.log('%c[->][precache]', styles.precache)
   return Promise.all(
-    CACHES.map(function({ name, files }) {
-      return caches.open(name).then(function(cache) {
-        return cache.addAll(files)
-      })
-    })
+    CACHES.map(({ name, files }) =>
+      caches.open(name).then(cache => cache.addAll(files))
+    )
   )
 }
 
@@ -143,8 +137,8 @@ function precache() {
  */
 function fromUsingCache(cacheName) {
   return function fromCache(request) {
-    return caches.open(cacheName).then(function(cache) {
-      return cache.match(request).then(function(matching) {
+    return caches.open(cacheName).then(cache =>
+      cache.match(request).then(matching => {
         if (DEBUG && matching) {
           console.log(
             `%c[<-][cache ${cacheName}]`,
@@ -154,7 +148,7 @@ function fromUsingCache(cacheName) {
         }
         return matching || Promise.reject('no-match')
       })
-    })
+    )
   }
 }
 
@@ -171,13 +165,13 @@ function updateUsingCache(cacheName) {
         styles.updateCache,
         request.url
       )
-    return caches.open(cacheName).then(function(cache) {
-      return fromNetwork(request).then(function(response) {
-        return cache.put(request, response.clone()).then(function() {
-          return response
-        })
-      })
-    })
+    return caches
+      .open(cacheName)
+      .then(cache =>
+        fromNetwork(request).then(response =>
+          cache.put(request, response.clone()).then(() => response)
+        )
+      )
   }
 }
 
@@ -190,7 +184,7 @@ function updateUsingCache(cacheName) {
  * If timeout is specified, throws an error if timeout exceeded
  */
 function fromNetwork(request, timeout) {
-  return new Promise(function(fulfill, reject) {
+  return new Promise((fulfill, reject) => {
     var timeoutId = timeout && setTimeout(reject, timeout)
     fetch(request).then(function(response) {
       if (DEBUG)
@@ -212,18 +206,21 @@ function fromNetwork(request, timeout) {
 function refresh(response) {
   if (DEBUG) console.log('%c[<-][refresh]', styles.refresh, response.url)
 
-  return self.clients.matchAll().then(function(clients) {
-    return Promise.all(
-      clients.map(function(client) {
-        return new Promise(function(resolve) {
-          var message = {
+  // Get all attached clients
+  return self.clients.matchAll().then(clients =>
+    Promise.all(
+      // For each client
+      clients.map(client =>
+        // Post message
+        client.postMessage(
+          // To refresh
+          JSON.stringify({
             type: 'refresh',
             url: response.url,
             eTag: response.headers.get('ETag')
-          }
-          resolve(client.postMessage(JSON.stringify(message)))
-        })
-      })
+          })
+        )
+      )
     )
-  })
+  )
 }
