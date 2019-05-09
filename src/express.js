469e02e1045bac93f03c8c2e37da77c84a41e0ab
David Higgins <thehig@gmail.com>
2019-05-09 22:24:03 +0100

    Rewrite with async/await

--- src/express.js
+++ src/express.js
@@ -21,17 +21,14 @@ app.set('views', 'src/views')
  * Get the streams from the database (`getStreams`) and render them using the `index` view template
  */
 app.get('/', async (req, res) => {
-  return getStreams()
-    .then(streams => {
-      res.render('index', {
-        streams
-      })
-    })
-    .catch(err => {
-      console.error(err)
-      res.statusCode = 500
-      res.send(`${err}`)
-    })
+  try {
+    const streams = await getStreams()
+    res.render('index', { streams })
+  } catch (err) {
+    console.error(err)
+    res.statusCode = 500
+    res.send(`${err}`)
+  }
 })
 
 /**
@@ -42,22 +39,24 @@ app.get('/', async (req, res) => {
  * - fetch the page by ID and scrape the scripts
  * - render scripts using the `stream` view template
  */
-app.get('/stream/:streamid', (req, res) => {
-  return incrementStreamCount(req.params.streamid)
-    .then(() =>
-      Promise.all([
-        fetchScript(req.params.streamid),
-        getStreamName(req.params.streamid)
-      ])
-    )
-    .then(([scripts, name]) => {
-      res.render('stream', { ...scripts, name })
-    })
-    .catch(err => {
-      console.error(err)
-      res.statusCode = 500
-      res.send(`${err}`)
-    })
+app.get('/stream/:streamid', async (req, res) => {
+  try {
+    const streamid = req.params['streamid']
+    if (!streamid) throw new Error(`Invalid streamid: ${streamid}`)
+
+    // Beware Promise.all fail-fast behavior!
+    const [count, scripts, name] = await Promise.all([
+      incrementStreamCount(streamid),
+      fetchScript(streamid),
+      getStreamName(streamid)
+    ])
+
+    res.render('stream', { ...scripts, name })
+  } catch (err) {
+    console.error(err)
+    res.statusCode = 500
+    res.send(`${err}`)
+  }
 })
 
 // Serve static files
--- src/fetch-script.js
+++ src/fetch-script.js
@@ -1,45 +1,49 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 
-module.exports = (streamid = 138) =>
-  axios
-    .get(`https://www.arconaitv.us/stream.php?id=${streamid}`)
-    .then(response => response.data)
-    .then(html => {
-      const $ = cheerio.load(html) // Populate cheerio, our headless DOM
-      const scripts = $('script')
-        .map(function() {
-          // For each script (Don't use an arrow fn as it won't have 'this')
-          //  get the source and the data from the script
-          const source = this.attribs['src']
-          const data =
-            this.children[0] && this.children[0].data
-              ? this.children[0].data
+module.exports = async (streamid = 138) => {
+  try {
+    const page = await axios.get(
+      `https://www.arconaitv.us/stream.php?id=${streamid}`
+    )
+
+    const $ = cheerio.load(page.data) // Populate cheerio, our headless DOM
+    const scripts = $('script')
+      .map(function() {
+        // For each script (Don't use an arrow fn as it won't have 'this')
+        //  get the source and the data from the script
+        const source = this.attribs['src']
+        const data =
+          this.children[0] && this.children[0].data
+            ? this.children[0].data
+            : undefined
+
+        // Reformat the script to be injected (if it contains 'video')
+        return {
+          inlineScript:
+            data && data.indexOf('video') >= 0
+              ? `<script>${data}</script>`
+              : undefined,
+          referenceScript:
+            source && source.indexOf('video') >= 0
+              ? `<script src="${source}"></script>`
               : undefined
+        }
+      })
+      .get()
 
-          // Reformat the script to be injected (if it contains 'video')
-          return {
-            inlineScript:
-              data && data.indexOf('video') >= 0
-                ? `<script>${data}</script>`
-                : undefined,
-            referenceScript:
-              source && source.indexOf('video') >= 0
-                ? `<script src="${source}"></script>`
-                : undefined
-          }
-        })
-        .get()
-
-      // Join the scripts together
-      return {
-        referenceScripts: scripts
-          .filter(script => script.referenceScript)
-          .map(script => script.referenceScript)
-          .join(''),
-        inlineScripts: scripts
-          .filter(script => script.inlineScript)
-          .map(script => script.inlineScript)
-          .join('')
-      }
-    })
+    // Join the scripts together
+    return {
+      referenceScripts: scripts
+        .filter(script => script.referenceScript)
+        .map(script => script.referenceScript)
+        .join(''),
+      inlineScripts: scripts
+        .filter(script => script.inlineScript)
+        .map(script => script.inlineScript)
+        .join('')
+    }
+  } catch (err) {
+    throw new Error(`Problem encountered while fetching scripts: ${err}`)
+  }
+}
